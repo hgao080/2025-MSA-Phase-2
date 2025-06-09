efb40cdd97d1d63af21ef7f3978710f9672cc71d
Henry Gao <henrygao48@gmail.com>
2025-06-10 11:41:28 +1200

    Template landing page with TailwindUI components

--- frontend/src/Pages/Landing.tsx
+++ frontend/src/Pages/Landing.tsx
@@ -1,9 +1,21 @@
-import Header from "../Components/Header";
+import Header from '../Components/Header';
+import Hero from '../Components/Hero';
+import Feature from '../Components/Feature'
 
 export default function Landing() {
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+	return (
+		<div>
+			<div className='absolute inset-x-0 top-0 z-50'>
+				<Header />
+			</div>
+
+			<div className=''>
+				<Hero />
+			</div>
+
+      <div className="">
+        <Feature />
+      </div>
+		</div>
+	);
 }
