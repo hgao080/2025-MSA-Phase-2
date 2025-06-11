6eac3f7ea85eba1780b880e4fd400cb625ce2079
Henry Gao <henrygao48@gmail.com>
2025-06-12 11:01:41 +1200

    Route button click if not logged in to login page

--- frontend/src/Components/SelectedProject.tsx
+++ frontend/src/Components/SelectedProject.tsx
@@ -1,17 +1,27 @@
-import { Button } from '@headlessui/react'
-
+import { useAuthStore } from "../Stores/AuthStore";
 import { useProjectStore } from "../Stores/ProjectStore";
 
+import { useNavigate } from "react-router";
+
 export default function SelectedProject() {
+  const navigate = useNavigate();
+
+  const user = useAuthStore(state => state.user);
   const project = useProjectStore(state => state.selectedProject);
+
+  const handleApply = () => {
+    if (!user) {
+      navigate("/login");
+    }
+  }
 
   return (
     <div className="flex flex-col shadow-xl rounded-3xl p-6">
       <div className="flex justify-between items-center">
         <h2>{project?.title}</h2>
-        <Button className="data-hover:cursor-pointer">
+        <button className="hover:cursor-pointer" onClick={handleApply}>
           Apply
-        </Button>
+        </button>
       </div>
       <p className="">{project?.description}</p>
     </div>
