971ba4e8db3cc2447cd815d30892671cca8fa446
Henry Gao <henrygao48@gmail.com>
2025-06-30 16:38:09 +1200

    Remove MUI setup from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,14 +7,9 @@ import App from './App.tsx';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import GlobalStyles from '@mui/material/GlobalStyles';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<StyledEngineProvider enableCssLayer>
-      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
 			<App />
-		</StyledEngineProvider>
 	</StrictMode>
 );
