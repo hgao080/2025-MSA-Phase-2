75888f82d45c468276185283bb25a89d7209c941
Henry Gao <henrygao48@gmail.com>
2025-06-30 16:04:57 +1200

    Configure frontend to change api url depending on environment

--- frontend/src/Services/apiClient.ts
+++ frontend/src/Services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosResponse } from 'axios';
 
 export const axiosClient = axios.create({
-	baseURL: `http://localhost:5006/api`,
+	baseURL: `${import.meta.env.VITE_API_URL}/api`,
 	headers: {
 		'Content-Type': 'application/json',
 	},
